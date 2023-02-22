bcf59e77d92ba49b68e7144a2e49822b13007d9e
Harsh_027 <111401544+kingo027@users.noreply.github.com>
2023-02-22 00:22:14 -0500

    Add files via upload

---
+++ js/contact.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $("#first_name").focus();
+
+    $("#contact").validate({
+        rules: {
+            first_name: {
+                required: true
+            },
+            last_name: {
+                required: true
+                
+            },
+	    Email: {
+                required: true,
+                email: true
+	    },
+            dob: {
+                required: true,
+		date: true
+            },
+            children: {
+                required: true
+            },
+            customRadioInline1: {
+                required: true
+            },
+            phone: {
+                required: true,
+                minlength:10,
+                digits: true
+            }
+        },
+        messages: {
+            nights: {
+                required: "Please enter number of nights.",
+                digits: "This is not a valid entry. Enter digits only."
+            }
+        }
+    })
+}); // end ready
---
+++ js/subset_expansion.js
@@ -0,0 +1,15 @@
+
+function functionA() {
+    document.getElementById('aId').innerText = document.getElementById('aId').innerText === "Show more" ? "Show less" : "Show more"
+    document.getElementById('aDiv').style.display = document.getElementById('aDiv').style.display === "block" ? "none" : "block"
+}
+
+function functionB() {
+    document.getElementById('aId1').innerText = document.getElementById('aId1').innerText === "Show more" ? "Show less" : "Show more"
+    document.getElementById('aDiv1').style.display = document.getElementById('aDiv1').style.display === "block" ? "none" : "block"
+}
+
+function functionC() {
+    document.getElementById('aId2').innerText = document.getElementById('aId2').innerText === "Show more" ? "Show less" : "Show more"
+    document.getElementById('aDiv2').style.display = document.getElementById('aDiv2').style.display === "block" ? "none" : "block"
+}
